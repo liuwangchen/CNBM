f180c0900dbe4a7cf13dea38b825b71cbe37f6ab
liuwangchen <liuwangchen@ciphertrading.com>
2018-07-18 18:05:51 +0800

    array

--- Main/main.go
+++ Main/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"CNBM/Container"
 	"fmt"
 )
 
-func main(){
-	fmt.Println("fdsfsd")
+func main() {
+	arr := Container.GetArray(10)
+	for i := 0; i < 10; i++ {
+		arr.AddLast(i)
+	}
+	fmt.Println(arr)
+
+	arr.Add(1, 100)
+	fmt.Println(arr)
+
+	arr.AddFirst(-1)
+	fmt.Println(arr)
 }
